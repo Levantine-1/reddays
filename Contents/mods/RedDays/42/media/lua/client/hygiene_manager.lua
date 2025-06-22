7192834abee6bd040b57445ec1f4fe63a3a3b28a
Levantine <github@levantine.io>
2025-06-22 00:25:46 -0700

    Rename display category for more generic item check by category, moved granular consume steps to helper function for clarity

--- Contents/mods/RedDays/42/media/lua/client/hygiene_manager.lua
+++ Contents/mods/RedDays/42/media/lua/client/hygiene_manager.lua
@@ -2,16 +2,20 @@ HygieneManager = {}
 
 local function consumeSanitaryItemHelperRenameItemAndLeakChance(item)
     local itemName = item:getName()
-    local current_condition = item:getCondition()
     print("Consuming sanitary item: " .. itemName)
 
-    baseName = itemName:match("^(.-) %(")
+    current_condition = item:getCondition()
+    if current_condition > 1 then
+        item:setCondition(current_condition - 1)
+        print("Sanitary item consumed, remaining condition: " .. item:getCondition())
+    end
 
+    baseName = itemName:match("^(.-) %(")
     if not baseName then
         baseName = itemName
     end
+
     d20Roll = ZombRand(1, 21) -- Chance to leak
-    print("d20 Roll: " .. d20Roll)
     if current_condition < 10 and current_condition > 5 then
         item:setName(baseName .. " (Bloody)")
     elseif current_condition == 5 then
@@ -41,23 +45,13 @@ end
 
 local function consumeSanitaryItem()
     local player = getPlayer()
-    -- Get all equipped clothing items
     local wornItems = player:getWornItems()
+
     for i = 0, wornItems:size() - 1 do
         local item = wornItems:get(i):getItem()
-        if item and item:getType() == "Sanitary_Pad" then
-            current_condition = item:getCondition()
-            if current_condition > 1 then
-                item:setCondition(current_condition - 1)
-                print("Sanitary item consumed, remaining condition: " .. item:getCondition())
-            else
-                print("Sanitary item is already at unusable condition.")
-            end
-            if not consumeSanitaryItemHelperRenameItemAndLeakChance(item) then
-                print("Sanitary item leaked.")
-                return false
-            end
-            return true
+        if item and item:getDisplayCategory() == "Feminine Hygiene" then
+            consumeSanitaryItem = consumeSanitaryItemHelperRenameItemAndLeakChance(item)
+            return consumeSanitaryItem
         end
     end
     print("No sanitary item equipped.")
