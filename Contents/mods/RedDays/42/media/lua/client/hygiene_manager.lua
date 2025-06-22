cd4da2408c5deb992c731a4f8f281b2ac10a9029
Levantine <github@levantine.io>
2025-06-21 22:31:07 -0700

    initial commit of pad degredation logic

--- Contents/mods/RedDays/42/media/lua/client/hygiene_manager.lua
+++ Contents/mods/RedDays/42/media/lua/client/hygiene_manager.lua
@@ -1,10 +1,41 @@
 HygieneManager = {}
 
+local function consumeSanitaryItem()
+    local player = getPlayer()
+    -- Get all equipped clothing items
+    local wornItems = player:getWornItems()
+    for i = 0, wornItems:size() - 1 do
+        local item = wornItems:get(i):getItem()
+        if item and item:getType() == "Sanitary_Pad" then
+            local itemName = item:getName()
+            print("Consuming sanitary item: " .. itemName)
+            current_condition = item:getCondition()
+            if current_condition > 1 then
+                item:setCondition(current_condition - 1)
+                print("Sanitary item consumed, remaining condition: " .. item:getCondition())
+            else
+                print("Sanitary item is already at unusable condition.")
+            end
+
+            if current_condition <= 3 then
+                item:setName(itemName .. " (Dirty)")
+            end
+            return true
+        end
+    end
+    print("No sanitary item equipped.")
+    return false
+end
+
 function HygieneManager.consumeHygieneProduct()
     local player = getPlayer()
     local stats = player:getStats()
     local bodyDamage = player:getBodyDamage()
     local groin = bodyDamage:getBodyPart(BodyPartType.Groin)
+
+    print("Attempting to consume hygiene product...")
+    consumeSanitaryItem()
+    print("Hygiene product consumed.")
 
     if groin:bandaged() then
         current_bandageLife = groin:getBandageLife()
