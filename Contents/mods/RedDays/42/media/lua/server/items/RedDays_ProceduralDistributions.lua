b92d9b168121cf489511d8ec6bb9b7add28670f3
Levantine <github@levantine.io>
2025-07-11 03:22:40 -0700

    Added sandbox option for spawn rate, increased spawn rate for certain locations, updated README

--- Contents/mods/RedDays/42/media/lua/server/items/RedDays_ProceduralDistributions.lua
+++ Contents/mods/RedDays/42/media/lua/server/items/RedDays_ProceduralDistributions.lua
@@ -1,219 +1,221 @@
+spawnRateMultiplier = SandboxVars.RedDays.hygiene_item_spawn_rate_multiplier or 1
+
 table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomCabinet"].items, 2 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2);
+table.insert(ProceduralDistributions["list"]["BathroomShelf"].items, 2 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 1 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserClassy"].items, 1 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["BedroomDresserRedneck"].items, 1 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartCosmetics"].items, 30 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10);
+table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10);
+table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10);
+table.insert(ProceduralDistributions["list"]["HospitalRoomWardrobe"].items, 10 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1);
+table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1.5 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1);
+table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1.5 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1);
+table.insert(ProceduralDistributions["list"]["OfficeDeskSecretary"].items, 1.5 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 10);
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 30 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 10);
+table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 10);
+table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 10);
+table.insert(ProceduralDistributions["list"]["StripClubDressers"].items, 20 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1);
+table.insert(ProceduralDistributions["list"]["WaitingRoomDesk"].items, 1 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1);
+table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1);
+table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1);
+table.insert(ProceduralDistributions["list"]["BathroomCounterMotel"].items, 1 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 10);
+table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 10);
+table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 10);
+table.insert(ProceduralDistributions["list"]["GasStoreToiletries"].items, 30 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 30 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartPaper"].items, 30 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 40 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 40 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartBathing"].items, 40 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 40 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 40 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 20);
+table.insert(ProceduralDistributions["list"]["GigamartToiletries"].items, 40 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 10);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 10);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 10);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical_Late"].items, 20 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5);
+table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5);
+table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5);
+table.insert(ProceduralDistributions["list"]["ChangeroomCounters"].items, 5 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20);
+table.insert(ProceduralDistributions["list"]["SafehouseMedical"].items, 20 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, "RedDays.PantyLinerBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 2);
+table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 10 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, "RedDays.SanitaryPadBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 2);
+table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 10 * spawnRateMultiplier);
 
 table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, "RedDays.TamponBox");
-table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 2);
+table.insert(ProceduralDistributions["list"]["HospitalRoomShelves"].items, 10 * spawnRateMultiplier);
 
 -- ----------------------------------------------------------------------------------------
