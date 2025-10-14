d7d3def505fe092882655775c8d63c1addaa9b65
Levantine <github@levantine.io>
2025-10-13 22:29:38 -0700

    abstracting main loop and adding moodle reset on replace/unequip

--- Contents/mods/RedDays/42/media/lua/client/moodles.lua
+++ Contents/mods/RedDays/42/media/lua/client/moodles.lua
@@ -1,5 +1,10 @@
 require "MF_ISMoodle"
 
+require "RedDays/hygiene_manager"
+require "RedDays/cycle_manager"
+
+local RedDaysMoodles = {}
+
 MF.createMoodle("DirtyPantyLiner");
 MF.createMoodle("BloodyPantyLiner");
 
@@ -11,36 +16,121 @@ MF.createMoodle("BloodyTampon");
 
 MF.createMoodle("Leak");
 
-local RedDaysMoodles = {}
+
 -- Moodle level is a float value between 0 and 1 where 0 is worst moodle state and 1 is best moodle state and .5 is neutral
+function RedDaysMoodles.setMoodle(name, level, playerID)
+    MF.getMoodle(name, playerID):setValue(level)
+end
+
+-- Determine if player bathed based on clothes/body dirtiness/bloody
+function getMoodleLevel(hygieneItemCondition)
+    local moodleLevel = 0.5
+    if hygieneItemCondition == 4 then
+        moodleLevel = 0.4
+    elseif hygieneItemCondition == 3 then
+        moodleLevel = 0.3
+    elseif hygieneItemCondition == 2 then
+        moodleLevel = 0.2
+    elseif hygieneItemCondition < 2 then
+        moodleLevel = 0
+    end
+    return moodleLevel
+end
 
 
+function getHygieneItemName(hygieneItem)
+    local hygieneItemName = hygieneItem:getType()
+    hygieneItemName = string.gsub(hygieneItemName, "_", "") -- Remove all underscores
+    return hygieneItemName
+end
 
-local MLevel = .50
-function flipFlopDebug()
-    local playerNum = getPlayer():getPlayerNum()
 
-    print("FlipFlopDebug - setting all moodles to " .. MLevel)
+local function getCurrentPlayerNum()
+    return getPlayer():getPlayerNum()
+end
 
-    MF.getMoodle("DirtyPantyLiner", playerNum):setValue(MLevel)
-    MF.getMoodle("BloodyPantyLiner", playerNum):setValue(MLevel)
+local function getCurrentHygieneItem()
+    return HygieneManager.getCurrentlyWornSanitaryItem()
+end
 
-    MF.getMoodle("DirtySanitaryPad", playerNum):setValue(MLevel)
-    MF.getMoodle("BloodySanitaryPad", playerNum):setValue(MLevel)
+local function getCurrentPhaseData()
+    return CycleManager.getPhaseStatus(modData.ICdata.currentCycle)
+end
 
-    MF.getMoodle("DirtyTampon", playerNum):setValue(MLevel)
-    MF.getMoodle("BloodyTampon", playerNum):setValue(MLevel)
+local function getMoodleType(phase)
+    return (phase == "redPhase") and "Bloody" or "Dirty"
+end
 
-    MF.getMoodle("Leak", playerNum):setValue(MLevel)
-
-    if MLevel < 0 then
-        print("Resetting to .50")
-        MLevel = .50
+local function getMoodleLevelForItem(moodletype, condition)
+    if moodletype == "Dirty" and condition <= 1 then
+        return 0.4 -- Only show level 1 moodle for dirty from discharge
     else
-        print("Decreasing by .10")
-        MLevel = MLevel - .10
+        return getMoodleLevel(condition)
     end
 end
-Events.EveryOneMinute.Add(flipFlopDebug)
+
+local function resetMoodles()
+    local playerNum = getCurrentPlayerNum()
+    local moodleNames = {
+        "DirtyPantyLiner",
+        "BloodyPantyLiner",
+        "DirtySanitaryPad",
+        "BloodySanitaryPad",
+        "DirtyTampon",
+        "BloodyTampon"
+    }
+    for _, name in ipairs(moodleNames) do
+        MF.getMoodle(name, playerNum):setValue(0.5)
+    end
+end
+
+function mainLoop()
+    print("RedDaysMoodles.mainLoop")
+    local hygieneItem = getCurrentHygieneItem()
+    if not hygieneItem then
+        print("No hygiene item found.")
+        return
+    end
+
+    print("Hygiene item found")
+    local playerNum = getCurrentPlayerNum()
+    local hygieneItemCondition = hygieneItem:getCondition()
+    local hygieneItemName = getHygieneItemName(hygieneItem)
+    print("Hygiene Item - " .. hygieneItemName .. " Condition - " .. hygieneItemCondition)
+
+    local phaseData = getCurrentPhaseData()
+    print("Current phase: " .. tostring(phaseData.phase) .. " Time remaining: " .. tostring(phaseData.time_remaining) .. " Percent complete: " .. tostring(phaseData.percent_complete))
+
+    local moodletype = getMoodleType(phaseData.phase)
+    local moodleLevel = getMoodleLevelForItem(moodletype, hygieneItemCondition)
+    local moodleName = moodletype .. hygieneItemName
+
+    print("Setting moodle - " .. moodleName .. " to level - " .. moodleLevel)
+    MF.getMoodle(moodleName, playerNum):setValue(moodleLevel)
+end
+Events.EveryOneMinute.Add(mainLoop)
+
+
+-- Below are intercept functions that are triggered when the player interacts with hygiene items.
+
+-- If player unequips the hygiene item, inspect the item and update the cycle tracker
+local o_ISUnequipAction_perform = ISUnequipAction.perform
+function ISUnequipAction:perform()
+    if self.item:getBodyLocation() == "HygieneItem" then
+        print("Resetting moodles due to unequip")
+        resetMoodles()
+    end
+    o_ISUnequipAction_perform(self)
+end
+
+-- If the player replaces a hygiene item, inspect the item and update the cycle tracker
+local o_ISWearClothing_perform = ISWearClothing.perform
+function ISWearClothing:perform()
+    if self.item:getBodyLocation() == "HygieneItem" then
+        print("Resetting moodles due to equipping a new hygiene item")
+        resetMoodles()
+    end
+    o_ISWearClothing_perform(self)
+end
 
 return RedDaysMoodles
