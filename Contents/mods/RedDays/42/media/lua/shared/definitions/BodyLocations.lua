5b136ddde16307cb1709067409686ee957c85d37
Levantine <github@levantine.io>
2026-01-16 22:02:42 -0800

    Update to new b42_13 item inventory system working poc

--- Contents/mods/RedDays/42/media/lua/shared/definitions/BodyLocations.lua
+++ Contents/mods/RedDays/42/media/lua/shared/definitions/BodyLocations.lua
@@ -1,5 +1,11 @@
--- BodyLocations are now registered in registries.lua using ItemBodyLocation.register()
--- Do not manually create them here in 42.13+
--- local group = BodyLocations.getGroup("Human")
--- local bodyLocation = BodyLocation.new(group, "HygieneItem")
--- group:getAllLocations():add(bodyLocation)
+-- BodyLocations are registered in registries.lua using ItemBodyLocation.register()
+-- But we still need to add them to the Human body location group
+
+local group = BodyLocations.getGroup("Human")
+
+local hygieneLocation = ItemBodyLocation.get(ResourceLocation.of("RedDays:HygieneItem"))
+if hygieneLocation then
+    group:getOrCreateLocation(hygieneLocation)
+else
+    print("WARNING: RedDays - Could not get HygieneItem body location - ItemBodyLocation.get() returned nil")
+end
