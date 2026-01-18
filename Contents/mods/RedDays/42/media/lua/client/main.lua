8190b16fc5a8d5a5dd8ee11ebaf9050df2cd5251
Levantine <github@levantine.io>
2026-01-17 21:46:10 -0800

    re-enabled main logic processing

--- Contents/mods/RedDays/42/media/lua/client/main.lua
+++ Contents/mods/RedDays/42/media/lua/client/main.lua
@@ -2,34 +2,6 @@ require "RedDays/cycle_manager"
 require "RedDays/effects_manager"
 require "RedDays/hygiene_manager"
 
--- local function setOldCycle() -- Test old cycle structure before publishing updates
---     local player = getPlayer()
---     if not player then return end
---     modData = player:getModData()
-
---     print("Setting old cycle values for testing purposes.")
---     oldCycle = {
---         cycle_start_day = getGameTime():getWorldAgeHours() / 24,
---         cycle_duration = 28,
---         follicular_duration = 14,
---         red_days_duration = 4,
---         follicle_stimulating_start_day = 5,
---         follicle_stimulating_duration = 10,
---         ovulation_duration = 1,
---         ovulation_day = 14,
---         luteal_start_day = 15,
---         luteal_duration = 14,
---         stiffness_target = 55,
---         stiffness_increment = 2,
---         discomfort_target = 100,
---         endurance_decrement = 0.0005,
---         fatigue_increment = 0.0001,
---         reason_for_cycle = "defaultCycle",
---         timeToDelaycycle = 0
---     }
---     modData.ICdata.currentCycle = oldCycle
--- end
--- Events.OnGameStart.Add(setOldCycle)
 
 local function LoadPlayerData()
 	local player = getPlayer()
@@ -41,7 +13,7 @@ local function LoadPlayerData()
         modData.ICdata.currentCycle = CycleManager.newCycle("LoadPlayerData_afterValidation")
     end
 end
--- Events.OnGameStart.Add(LoadPlayerData)
+Events.OnGameStart.Add(LoadPlayerData)
 
 local function phaseIsValid(phase)
     local valid_phases = {"delayPhase", "redPhase", "follicularPhase", "ovulationPhase", "lutealPhase"}
@@ -66,4 +38,4 @@ local function main()
     EffectsManager.determineEffects(cycle) -- Apply effects based on the current cycle phase
     CycleDebugger.printWrapper(cycle)
 end
--- Events.EveryTenMinutes.Add(main)
+Events.EveryTenMinutes.Add(main)
