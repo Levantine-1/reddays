06c2ca5abc501aefaaadb348cdfe399206e03f5f
Levantine <github@levantine.io>
2026-01-18 19:15:21 -0800

    Making food craving effect to be affected by input target value in a linear curve

--- Contents/mods/RedDays/42/media/lua/client/effects_pms.lua
+++ Contents/mods/RedDays/42/media/lua/client/effects_pms.lua
@@ -109,7 +109,12 @@ EffectsPMS = {}
             setFoodCravingEffect_jumpedToHungry = false
         end
 
-        if currentHunger > 0.01 and currentHunger < 0.16 and not setFoodCravingEffect_jumpedToHungry then
+        -- Linear mapping: input 0 → 0.1, input 100 → 0.001
+        local target_trigger_hunger_value = 0.1 - (target_value * 0.00099)
+        print("Food Craving Effect Input Target: " .. tostring(target_value) .. " Calculated Target Value: " .. tostring(target_trigger_hunger_value))
+
+        if currentHunger > target_trigger_hunger_value and currentHunger < 0.16 and not setFoodCravingEffect_jumpedToHungry then
+            print("Jumping hunger to peckish threshold due to food craving effect.")
             stats:set(CharacterStat.HUNGER, 0.16)  -- Jump to peckish threshold
             setFoodCravingEffect_jumpedToHungry = true
         end
@@ -174,7 +179,6 @@ EffectsPMS = {}
         local target_value = (pms_severity / 100) * currentCycle.healthEffectSeverity
 
         if modData.ICdata.pill_recently_taken then
-            print("PMS Painkiller Effect Active - Dropping Stiffness")
             clearStiffness(player, currentCycle)
         end
 
